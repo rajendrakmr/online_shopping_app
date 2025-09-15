0cc18504448efedfc8553e8f9e54baf086dff0bc
rajendra <rojendra@ubuntu.lan>
2025-09-15 18:20:19 +0530

    Change init container script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
   set -e
 
   # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+  docker pull rk0617/online-shop-app-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 rk0617/online-shop-app-demo:latest
